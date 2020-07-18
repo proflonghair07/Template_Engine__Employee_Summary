c7b65935a526b8673434c0bdbb88bdad7aae0b33
omniclause <vaughnmurphy89@gmail.com>
2020-07-18 17:58:15 -0500

    mostly finished, maybe a little more polishing

---
+++ README.md
@@ -0,0 +1,13 @@
+# Template Engine - Employee Summary
+
+## Images
+
+## ![](assets/cli_gif.gif)
+
+## ![](assets/tests_screenshot.png)
+
+## ![](assets/deployed_site.png)
+
+## Application Description
+
+## This application is a command line interface that generates an html page of employees using node.js. The user first runs app.js and is given a series of prompts that ask information about the employee being entered. After this series of prompts is executed the user is asked if they have another employee to enter information for. If the user indicates yes the prompts are run through again, if the user indicates no the html is generated.
--- app.js
+++ app.js
@@ -10,26 +10,97 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+const employeeArray = [];
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+prompts = [
+  {
+    type: "input",
+    message: "What is this employee's name?",
+    name: "name",
+  },
+  {
+    type: "list",
+    message: "What role does this employee have?",
+    name: "role",
+    choices: ["Engineer", "Intern", "Manager"],
+  },
+  {
+    type: "input",
+    message: "What is this employee's GitHub username?",
+    name: "github",
+    when: function (answers) {
+      return answers.role == "Engineer";
+    },
+  },
+  {
+    type: "input",
+    message: "What school is their internship thorugh?",
+    name: "school",
+    when: function (answers) {
+      return answers.role == "Intern";
+    },
+  },
+  {
+    type: "input",
+    message: "What is this employee's office number?",
+    name: "officeNumber",
+    when: function (answers) {
+      return answers.role == "Manager";
+    },
+  },
+  {
+    type: "input",
+    message: "What is this employee's id number?",
+    name: "idNumber",
+  },
+  {
+    type: "input",
+    message: "What is this employee's email address?",
+    name: "email",
+  },
+];
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
+function inquirerPrompt() {
+  return inquirer.prompt(prompts).then((answers) => {
+    const name = answers.name;
+    const role = answers.role;
+    const id = answers.idNumber;
+    const email = answers.email;
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
+    if (role == "Engineer") {
+      var newEmployee = new Engineer(name, id, email, answers.github);
+    } else if (role == "Intern") {
+      var newEmployee = new Intern(name, id, email, answers.school);
+    } else if (role == "Manager") {
+      var newEmployee = new Manager(name, id, email, answers.officeNumber);
+    }
+    employeeArray.push(newEmployee);
+    addEmployee();
+  });
+}
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
+function addEmployee() {
+  inquirer
+    .prompt({
+      type: "confirm",
+      message: "Do you have another employee to add?",
+      name: "addEmployee",
+    })
+    .then((answers) => {
+      if (answers.addEmployee) {
+        inquirerPrompt();
+      } else {
+        writeHTML(render(employeeArray));
+      }
+    });
+}
 
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+function writeHTML(template) {
+  if (!fs.existsSync(OUTPUT_DIR)) {
+    fs.mkdirSync(OUTPUT_DIR);
+  }
+  fs.writeFileSync(outputPath, template);
+  console.log("Page Rendered!");
+}
+
+inquirerPrompt();
